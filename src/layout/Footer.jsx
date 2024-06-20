bf699bd29337cfbbdf0a9d6b61f6470d94793a38
ashrafulrahmansakil <ashrafulrahmansakil@gmail.com>
2024-06-20 22:57:57 +0600

    school-management

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,13 @@
-
-
 const Footer = () => {
   let year = new Date();
 
   return (
-    <div className="bg-dark text-white p-2">
+    <div className="bg-dark text-white p-1">
       <div className="container text-center">
-        <span className="text-capitalize">Copyrignt &copy; {year.getFullYear()} All rights reserved </span>
+        <span className="text-capitalize">
+          Copyrignt &copy; {year.getFullYear()} All rights reserved
+        </span>
+        <p>Powerd by ashraful rahman sakil</p>
       </div>
     </div>
   );
